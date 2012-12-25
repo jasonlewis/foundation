8eae7d060f8ee106be5a8e988a44d0ca293ab3c0
Taylor Otwell <taylorotwell@gmail.com>
2012-12-25 13:58:56 -0600

    add the artisan serve command.

---
+++ src/Illuminate/Foundation/Console/ServeCommand.php
@@ -0,0 +1,48 @@
+<?php namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+
+class ServeCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'serve';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Serve the application on the PHP development server.";
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		chdir($this->app['path.base']);
+
+		$port = $this->input->getOption('port');
+
+		passthru("php -S localhost:{$port} -t public server.php");
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', 8000),
+		);
+	}
+
+}
