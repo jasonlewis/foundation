4f31a9d980d002c9bfba9123edc653c866d850b6
Taylor Otwell <taylorotwell@gmail.com>
2012-12-27 20:22:37 -0600

    define version.

--- src/start.php
+++ src/start.php
@@ -2,6 +2,19 @@
 
 /*
 |--------------------------------------------------------------------------
+| Define The Laravel Version
+|--------------------------------------------------------------------------
+|
+| Here we will set the Laravel version that is utilized to identify this
+| installation of the framework. It is primarily used via the console
+| to display the version to the developer for information purposes.
+|
+*/
+
+define('LARAVEL_VERSION', '4.0.0');
+
+/*
+|--------------------------------------------------------------------------
 | Register Class Imports
 |--------------------------------------------------------------------------
 |
